20044a721e8c01c47d7a34cf940dd019bdf80485
Deepansh <namdevdeepansh5@gmail.com>
2026-02-22 14:46:56 +0530

    Correct import path for userDataContext

--- frountend/src/pages/Home.jsx
+++ frountend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState, useContext } from "react";
-import { userDataContext } from "../context/userContext";
+import { userDataContext } from "../context/UserContext";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import AI from "../assets/AI.gif";
